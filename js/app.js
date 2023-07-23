b79980467cc74cb8a432ddb7c4dd13affd5efcc8
Teboho M <teboho.motsamai@Opsi.co.za>
2023-07-24 00:22:15 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,72 @@
+// Get elements from the DOM
+const gestureImage = document.getElementById('gesture-image');
+const captureBtn = document.getElementById('capture-btn');
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const capturedImage = document.getElementById('capturedImage');
+const feedBack = document.getElementById('feedback');
+// Dummy data for demonstration (replace with actual SASL gesture data)
+const saslGestures = [
+  { letter: 'A', imageUrl: 'C:/Users/Teboho Motsamai/Documents/Honours/CSIS6809/SASLHandGestureApp/images/A_HAND.png' },
+  { letter: 'E', imageUrl: 'C:/Users/Teboho Motsamai/Documents/Honours/CSIS6809/SASLHandGestureApp/images/E_HAND.png' },
+  { letter: 'I', imageUrl: 'C:/Users/Teboho Motsamai/Documents/Honours/CSIS6809/SASLHandGestureApp/images/I_HAND.png' },
+  { letter: 'O', imageUrl: 'C:/Users/Teboho Motsamai/Documents/Honours/CSIS6809/SASLHandGestureApp/images/O_HAND.png' },
+  { letter: 'U', imageUrl: 'C:/Users/Teboho Motsamai/Documents/Honours/CSIS6809/SASLHandGestureApp/images/U_HAND.png' },
+
+];
+
+// Function to randomly select a gesture and display it on the page
+function displayRandomGesture() {
+  const randomIndex = Math.floor(Math.random() * saslGestures.length);
+  const randomGesture = saslGestures[randomIndex];
+  gestureImage.src = randomGesture.imageUrl;
+  gestureImage.alt = `SASL Hand Gesture for ${randomGesture.letter}`;
+  gestureImage.style.display = 'block';
+}
+
+// Event listener for the "Capture Image" button
+captureBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    captureImage();
+    setTimeout(() => {
+      feedBack.textContent = "loading accuracy";
+      displayRandomGesture(); 
+    }, 1000);
+  }, 1000);
+});
+
+displayRandomGesture();
+
+// Check if getUserMedia is supported by the browser
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            // Set the video stream as the source for the video element
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function(error) {
+            console.error('Error accessing the camera: ', error);
+        });
+} else {
+    console.error('getUserMedia is not supported in this browser.');
+}
+
+// Function to capture the image from the video stream
+function captureImage() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    
+    // Convert the canvas content to a data URL and display it as an image
+    capturedImage.src = canvas.toDataURL('image/jpeg');
+    capturedImage.style.display = 'block';
+    capturedImage.style.width = "300px"
+    capturedImage.style.height = "300px"
+}
+
+// Add click event listener to the capture button
+captureBtn.addEventListener('click', function() {
+   
+
+});
